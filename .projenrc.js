7ccec651d9c06fa822d33aee7f96f259e6a5b858
Neil Guan <guan840912@gmail.com>
2021-03-21 13:17:21 +0800

    add projen-automate-it

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,10 @@
 const { AwsCdkConstructLibrary } = require('projen');
+const { Automation } = require('projen-automate-it');
 
 const PROJECT_NAME = 'cdk-aurora-globaldatabase';
 const PROJECT_DESCRIPTION = 'cdk-aurora-globaldatabase is an AWS CDK construct library that provides Cross Region Create Global Aurora RDS Databases.';
 const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
-const CDK_VERSION = '1.84.0';
+const CDK_VERSION = '1.94.1';
 
 const project = new AwsCdkConstructLibrary({
   name: PROJECT_NAME,
@@ -17,7 +18,6 @@ const project = new AwsCdkConstructLibrary({
     announce: false,
   },
   compat: true,
-  projenUpgradeSecret: AUTOMATION_TOKEN,
   cdkVersion: CDK_VERSION,
   stability: 'experimental',
   dependabot: false,
@@ -32,51 +32,21 @@ const project = new AwsCdkConstructLibrary({
     '@aws-cdk/custom-resources',
     '@aws-cdk/aws-iam',
   ],
+  deps: ['projen-automate-it'],
+  bundledDeps: ['projen-automate-it'],
   python: {
     distName: 'cdk-aurora-globaldatabase',
     module: 'cdk_aurora_globaldatabase',
   },
+  rebuildBot: false,
 });
 
-// create a custom projen and yarn upgrade workflow
-workflow = project.github.addWorkflow('ProjenYarnUpgrade');
-
-workflow.on({
-  schedule: [{
-    cron: '11 0 * * *',
-  }], // 0:11am every day
-  workflow_dispatch: {}, // allow manual triggering
+const automation = new Automation(project, {
+  automationToken: AUTOMATION_TOKEN,
 });
-
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
-});
+automation.autoApprove();
+automation.autoMerge();
+automation.projenYarnUpgrade();
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'image', 'yarn-error.log', 'coverage', 'venv'];
 const common_exclude_image = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'coverage', 'venv'];
